697a31006337f12231c7e58ebd797df7d201747a
Doomsdaybear <101878029+Doomsdaybear@users.noreply.github.com>
2022-05-23 08:53:41 -0400

    Create level2.cpp

---
+++ level2.cpp
@@ -0,0 +1,33 @@
+//Welcome back! In the previous tutorial, we covered the bare basics of C++ such as headers, libraries, functions and cout.
+//In this tutorial, we will be covering functions. They are written similarly to int main(), but instead of int, we use void.
+//So our code will look something like this:
+
+//#include <iostream>
+//using namespace std;
+//
+//void Myfunction() {
+// cout<<"Hello,world";
+// }
+//int main() {
+//Myfunction();
+//
+//return 0;
+//}
+//now,let's write that.
+
+#include <iostream>
+
+using namespace std;
+
+void Myfunction() {
+cout<<"Hello,world";
+}
+
+int main() {
+Myfunction();
+  
+return 0;
+}
+//Run this, and the output should be Hello,world!
+
+//This ends our tutorial, see you next time!
