75348c011b175b3dd1579921a013b6341008e666
Doomsdaybear <101878029+Doomsdaybear@users.noreply.github.com>
2022-05-23 08:45:43 -0400

    Create level1.cpp

---
+++ level1.cpp
@@ -0,0 +1,12 @@
+//Welcome! This is the first step in our C++ tutorial! Feel free to use code from these files as help for future projects.
+//As you may have noticed, comments are followed by backslashes. This is part of syntax for many(but not all) programming languages.
+//Now, lets start coding!
+#include <iostream> // This statement is called a header. Here, we are calling the iostream library. io(or i/o) stands for input/output, so really all this does is turn our code into output.
+
+using namespace std; // This is-unlike iostream- optional. Without it, you have to write things like std::cout. You don't need this but I prefer it because it makes writing code a tiny bit quicker.
+
+int main() { //here is where the action happens. The main integer is where the computer reads your code. Here we can do things like load functions (blocks of code) or just write code.
+cout<<"Hello,world!"; // writes hello world in the output
+
+return 0; // returns 0.
+}
